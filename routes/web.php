1c259a5f99d378176e3e96ce464c55e9be8f6aac
Daniel Kingsley (U1973914) <U1973914@unimail.hud.ac.uk>
2022-10-10 12:15:15 +0100

    Added edit and create pages. Improved frontend via tailwind.

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\VisitorController;
 Route::get('/', [WelcomeController::class, 'index']);
+Route::resource('/visitors', VisitorController::class);
