e281fcf183aef19e0c775f23801f5c390325fdc9
Daniel Kingsley (U1973914) <U1973914@unimail.hud.ac.uk>
2022-10-17 11:21:44 +0100

    Add user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,12 +2,13 @@
 namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Database\Seeders\VisitorSeeder;
+use Database\Seeders\UserSeeder;
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
         $this->call([
-            VisitorSeeder::class
+            UserSeeder::class
         ]);
     }
 }
