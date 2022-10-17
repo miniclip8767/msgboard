696321239f6b05d47f691fe0453f0e6a70130b3f
Daniel Kingsley (U1973914) <U1973914@unimail.hud.ac.uk>
2022-10-17 11:34:11 +0100

    Add login view/controller/routes

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\VisitorController;
+use App\Http\Controllers\LoginController;
 Route::get('/', [WelcomeController::class, 'index']);
 Route::resource('/visitors', VisitorController::class);
+Route::get('/login', [LoginController::class, 'login'])->name('login');
+Route::post('/login', [LoginController::class, 'authenticate'])->name('authenticate');
+
